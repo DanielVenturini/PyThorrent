d30f851f41ae69203fc44dd80162a37d63630f74
Daniel Venturini <daniellagarto0@gmail.com>
2018-06-12 09:28:00 -0300

    Logica de criacao de arquivos

--- THP_PWP/CommonDef.py
+++ THP_PWP/CommonDef.py
@@ -1,6 +1,7 @@
 # -*- conding:ISO-8859-1 -*-
 
 from random import randint
+import os
 
 def createPeerId():
     peerId = '-PT1012-'
@@ -27,21 +28,27 @@ def getPeerId():
 def openAndRead(fileName='', line=0, at=0):
     file = open(fileName)
 
-    if(line == -1):
-        return file.readlines()
+    try:
+        if(line == -1):
+            return file.readlines()
 
-    # jump to line specify
-    for i in range(0, line):
-        file.readline()
+        # jump to line specify
+        for i in range(0, line):
+            file.readline()
 
-    if(at == -1):
-        return file.readline().replace('\n', '')
-    else:
-        return file.read(at)
+        if(at == -1):
+            return file.readline().replace('\n', '')
+        else:
+            return file.read(at)
+    finally:
+        file.close()
 
 # if the file configures/configure.pt not exists
 # create it and insert in first line the peer id
 def createAndInsertPeerId():
+    if not os.path.exists('../configures/'):
+        os.makedirs('../configures/')
+
     peerId = createPeerId()
 
     configureFile = open('../configures/configure.pt', 'w+')
